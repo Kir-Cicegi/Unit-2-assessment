3231ec95fdfb1ae727ed1b7e11b636c60db1d504
Kir-Cicegi <84298571+Kir-Cicegi@users.noreply.github.com>
2021-06-25 15:15:51 -0400

    final edit

--- models/todo.js
+++ models/todo.js
@@ -10,13 +10,12 @@ function getAll() {
     return todos;
 }
 
-function create(todo) {
-    todo.id = Date.now() % 1000000;
-    todo.done = false;
-    todos.push(todo);
+function create(item) {
+    item.id = Date.now() % 1000000;
+    item.done = false;
+    todos.push(item);
 }
 
 function deleteItem(id) {
-    const x = todos.findIndex(t => t.id === parseInt(id));
-    todos.splice(x, 1);
+    todos.splice(todos.findIndex(t => t.id === parseInt(id)), 1);
 }
